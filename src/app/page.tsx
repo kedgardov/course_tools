4463eee25e1e0e49d9079c2406a4b08e7008a3f5
Kevin Trejo <kedgardov@gmail.com>
2024-07-22 18:34:08 -0700

    nuevo texto prueba github action

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
           <code className="font-mono font-bold">src/app/page.tsx</code>
         </p>
 	<p>
-      Testing
+      Prueba de github actions gratuitas para control de vm de produccion
 	</p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
           <a
