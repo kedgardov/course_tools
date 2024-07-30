67a0a5af7bdccae25fc311c7c3fad3c17d8ea020
Kevin Trejo <kedgardov@gmail.com>
2024-07-29 23:34:46 -0700

    fixed slice/action

--- src/features/login/loginActions.ts
+++ src/features/login/loginActions.ts
@@ -1,15 +1,26 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
-import { loginForm_t } from '../../models/forms/login';
+// import { createAsyncThunk } from '@reduxjs/toolkit';
+// import axios from 'axios';
+// import { loginForm_t } from '../../models/forms/login';
+// const API_ROUTE : string = 'http://132.145.196.46/api/';
 
-export const tryLogin = createAsyncThunk(
-    'login/tryLogin',
-    async (loginData: loginForm_t, { rejectWithValue }) => {
-        try {
+// interface credentialsCorrect
 
-        } catch (error) {
-
-        }
-    }
-
-);
+// export const tryLogin = createAsyncThunk(
+//     'login/tryLogin',
+//     async (loginData: loginForm_t, { rejectWithValue }) => {
+//         try {
+//             const { data, status } = await axios.post<boolean>(
+//                 API_ROUTE+'login/login',{
+//                     loginData,
+//                 },{
+//                     withCredentials: true,
+//                 }
+//             );
+//             if( data !== true ){
+//                 rejectWithValue('Credenciales Incorrectas');
+//             }
+//         } catch (error) {
+//             rejectWithValue('Problemas con el internet');
+//         }
+//     }
+// );
