799ba8d23992008758d91ce10129c42b686fcf0d
Kevin Trejo <kedgardov@gmail.com>
2024-08-28 20:14:34 -0700

    Added env var support

--- src/requestHandler/index.tsx
+++ src/requestHandler/index.tsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const requestHandler = axios.create({
-    baseURL: 'http://localhost/api/',
+    //baseURL: 'http://localhost/api/',
+    baseURL: process.env.NEXT_PUBLIC_BASE_URL,
     timeout: 4000,
     withCredentials: true,
 });
